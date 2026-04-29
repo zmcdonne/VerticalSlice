0ae27ae8963c0a3464d1dde7143f7c7d327088dd
Zoya <zmcdonne@uci.edu>
2026-04-28 23:43:18 -0700

    scene changes

---
+++ verticle slice/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+//public void Quit()
+  //  {
+ //       Application.Quit();
+ //   }
+}
